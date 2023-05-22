22220aee60c35fe037ca3ddd3471836a182c8152
Paulo Mantovani <mantovaniiip@gmail.com>
2023-05-21 22:02:46 -0300

    teste crud csharp
    crud
    .net  .net 5.0
    Entity Framework
    Banco de dados SQL server

---
+++ crud7/Models/BancoDeDados.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crud7.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Crud7;Integrated Security=True");
+        }
+    }
+}
---
+++ crud7/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace crud7.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; } 
+
+        public string Telefone { get; set; } 
+    }
+}
